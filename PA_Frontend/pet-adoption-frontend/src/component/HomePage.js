e31633e261eb93abfa4a476000a598f28cdb2813
sara-gaballa <saragaballa2002@gmail.com>
2023-12-25 23:56:54 +0200

    pages

--- PA_Frontend/pet-adoption-frontend/src/component/HomePage.js
+++ PA_Frontend/pet-adoption-frontend/src/component/HomePage.js
@@ -1,211 +1,70 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import Button from '@mui/material/Button'; // Change Link to Button
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CardActionArea from '@mui/material/CardActionArea';
-import TextField from '@mui/material/TextField';
-
+import Home from './Home';
+import Profile from './Profile';
 
 const sections = [
-  { title: 'Home', url: './HomePage' },
-
-];
-
-const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random?cats',
-  imageText: 'main image description',
-};
-
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random?cats',
-    imageLabel: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random?cats',
-    imageLabel: 'Image Text',
-  },
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random?cats',
-    imageLabel: 'Image Text',
-  },
+  { title: 'Home', component: <Home /> },
+  { title: 'Profile', component: <Profile /> },
 ];
 
 const defaultTheme = createTheme();
 
-export default function HomePage() {
-  const MainFeaturedPost = (props) => {
-    const { post } = props;
+const Header = (props) => {
+  const { sections, title, selectedSection, setSelectedSection } = props;
 
-    return (
-
-      <Paper
-        sx={{
-          position: 'relative',
-          backgroundColor: 'grey.800',
-          color: '#fff',
-          mb: 4,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundImage: `url(${post.image})`,
-        }}
-      >
-        {<img style={{ display: 'none' }} src={post.image} alt={post.imageText} />}
-        <Box
-          sx={{
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            backgroundColor: 'rgba(0,0,0,.3)',
-          }}
-        />
-        <Grid container>
-          <Grid item md={6}>
-            <Box
-              sx={{
-                position: 'relative',
-                p: { xs: 3, md: 6 },
-                pr: { md: 0 },
-              }}
-            >
-              <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                {post.title}
-              </Typography>
-              <Typography variant="h5" color="inherit" paragraph>
-                {post.description}
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Paper>
-    );
-  };
-
-  const Header = (props) => {
-    const { sections, title } = props;
-
-    return (
-      <React.Fragment>
-        <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-         
-          <Typography
-            component= "h2"
-            variant="h5"
-            color="inherit"
-            align="center"
-            noWrap
-            sx={{ flex: 1 }}
-          >
-            {title}
-          </Typography>
-        </Toolbar>
-        <Toolbar
-          component="nav"
-          variant="dense"
-          sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
         >
-          {sections.map((section) => (
-            <Link
-              color="inherit"
-              noWrap
-              key={section.title}
-              variant="body2"
-              href={section.url}
-              sx={{ p: 1, flexShrink: 0 }}
-            >
-              {section.title}
-            </Link>
-          ))}
-        </Toolbar>
-      </React.Fragment>
-    );
-  };
+          {title}
+        </Typography>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        {sections.map((section, index) => (
+          <Button
+            key={section.title}
+            color="inherit"
+            variant="text"
+            onClick={() => setSelectedSection(index)}
+            sx={{ p: 1, flexShrink: 0, ...(selectedSection === index && { backgroundColor: '#e0e0e0' }) }}
+          >
+            {section.title}
+          </Button>
+        ))}
+      </Toolbar>
+    </React.Fragment>
+  );
+};
 
-  const FeaturedPost = (props) => {
-    const { post } = props;
-
-    return (
-      <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href="./PetDetailPage">
-        <Card sx={{ display: 'flex' }}>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography component="h2" variant="h5">
-              {post.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {post.date}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-          </CardContent>
-          <CardMedia
-            component="img"
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-            image={post.image}
-            alt={post.imageLabel}
-          />
-        </Card>
-      </CardActionArea>
-    </Grid>
-    );
-  };
+export default function HomePage() {
+  const [selectedSection, setSelectedSection] = React.useState(0);
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Pet Adoption" sections={sections} />
-        <main>
-          <MainFeaturedPost post={mainFeaturedPost} />
-
-          <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: 4}}>
-          <TextField
-            label="Search"
-            variant="outlined"
-            sx={{ width: '100%', maxWidth: '450px', marginRight: 90 }}
-          />
+        <Header title="Pet Adoption" sections={sections} selectedSection={selectedSection} setSelectedSection={setSelectedSection} />
+        <Box mt={2}>
+          {sections[selectedSection].component}
         </Box>
-
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
-
-          <hr style={{ margin: '20px 0', borderTop: '1px solid #ccc' }} />
-
-        </main>
       </Container>
     </ThemeProvider>
   );
