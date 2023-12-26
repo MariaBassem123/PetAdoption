a8c171fd24c4298b13d0f8ca150af9acdd1248fd
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-26 12:05:00 +0200

    Staff controller

--- PA_Backend/PetAdoptionSystem/src/main/java/com/example/PetAdoptionSystem/controller/StaffController.java
+++ PA_Backend/PetAdoptionSystem/src/main/java/com/example/PetAdoptionSystem/controller/StaffController.java
@@ -40,45 +40,57 @@ public class StaffController {
         }
     }
 
-    @GetMapping("/getByStaffId")
-    public ResponseEntity<Staff> getByStaffId(@RequestParam int id) {
-        try {
-            Staff staff = staffService.getByStaffId(id);
-            if (staff != null) {
-                return ResponseEntity.ok(staff);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-            }
-        } catch (EmptyResultDataAccessException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+//    @GetMapping("/getByStaffId")
+//    public ResponseEntity<Staff> getByStaffId(@RequestParam int id) {
+//        try {
+//            Staff staff = staffService.getByStaffId(id);
+//            if (staff != null) {
+//                return ResponseEntity.ok(staff);
+//            } else {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//            }
+//        } catch (EmptyResultDataAccessException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        } catch (DataAccessException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
+//
+//    @GetMapping("/getByShelterId")
+//    public ResponseEntity<List<Staff>> getByShelterId(@RequestParam int id) {
+//        try {
+//            List<Staff> staffs = staffService.getByShelterId(id);
+//            return ResponseEntity.ok(staffs);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
+//
+//    @GetMapping("/getByName")
+//    public ResponseEntity<Staff> getByStaffName(@RequestParam String name) {
+//        try {
+//            Staff staff = staffService.getByStaffName(name);
+//            if (staff != null) {
+//                return ResponseEntity.ok(staff);
+//            } else {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//            }
+//        } catch (EmptyResultDataAccessException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        } catch (DataAccessException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
+
+    @GetMapping("/getStaffByEmail")
+    public Staff getStaffByEmail(@RequestParam String email, @RequestParam String password) {
+        return staffService.getStaff(email, password);
     }
 
-    @GetMapping("/getByShelterId")
-    public ResponseEntity<List<Staff>> getByShelterId(@RequestParam int id) {
-        try {
-            List<Staff> staffs = staffService.getByShelterId(id);
-            return ResponseEntity.ok(staffs);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
-
-    @GetMapping("/getByName")
-    public ResponseEntity<Staff> getByStaffName(@RequestParam String name) {
-        try {
-            Staff staff = staffService.getByStaffName(name);
-            if (staff != null) {
-                return ResponseEntity.ok(staff);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-            }
-        } catch (EmptyResultDataAccessException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+    @GetMapping("/checkStaff")
+    public ResponseEntity<String> checkStaff(@RequestParam String email, @RequestParam String password) {
+        System.out.println("In controller: email = "+ email + ", password = " + password);
+        StaffService.LoginStatus loginStatus = staffService.checkStaff(email, password);
+        return ResponseEntity.ok(loginStatus.name());
     }
 }
