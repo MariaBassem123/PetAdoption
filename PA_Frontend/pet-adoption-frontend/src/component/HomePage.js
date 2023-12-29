9a4240c6429d9ffb0eb6c215a41dde9cd836be39
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-12-29 20:36:31 +0200

    send user

--- PA_Frontend/pet-adoption-frontend/src/component/HomePage.js
+++ PA_Frontend/pet-adoption-frontend/src/component/HomePage.js
@@ -12,63 +12,63 @@ import ShelterManagement from './Shelter_Management';
 import { useNavigate, useParams  } from 'react-router-dom';
 
 
-const manager = [
-  { title: 'Home', component: <Home /> },
-  { title: 'Shelter Management', component: <ShelterManagement /> }, 
-];
-const staff = [
-  { title: 'Home', component: <Home /> },
-  { title: 'Profile', component: <StaffProfile /> },
-];
-const adaptor = [
-  { title: 'Home', component: <Home /> },
-];
-
-const defaultTheme = createTheme();
-
-const Header = (props) => {
-  const { sections, title, selectedSection, setSelectedSection } = props;
-
-  return (
-    <React.Fragment>
-      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          sx={{ flex: 1 }}
-        >
-          {title}
-        </Typography>
-      </Toolbar>
-      <Toolbar
-        component="nav"
-        variant="dense"
-        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
-        {sections.map((section, index) => (
-          <Button
-            key={section.title}
-            color="inherit"
-            variant="text"
-            onClick={() => setSelectedSection(index)}
-            sx={{ p: 1, flexShrink: 0, ...(selectedSection === index && { backgroundColor: '#e0e0e0' }) }}
-          >
-            {section.title}
-          </Button>
-        ))}
-      </Toolbar>
-    </React.Fragment>
-  );
-};
-
 const HomePage = () => {
   const { adopterData } = useParams();
   const decodedAdopterData = decodeURIComponent(adopterData);
   const parsedAdopterData = JSON.parse(atob(decodedAdopterData));
 
+  const manager = [
+    { title: 'Home', component: <Home user={parsedAdopterData}/> },
+    { title: 'Shelter Management', component: <ShelterManagement user={parsedAdopterData} /> }, 
+  ];
+  const staff = [
+    { title: 'Home', component: <Home user={parsedAdopterData} /> },
+    { title: 'Profile', component: <StaffProfile user={parsedAdopterData}/> },
+  ];
+  const adaptor = [
+    { title: 'Home', component: <Home user={parsedAdopterData} /> },
+  ];
+  
+  const defaultTheme = createTheme();
+  
+  const Header = (props) => {
+    const { sections, title, selectedSection, setSelectedSection } = props;
+  
+    return (
+      <React.Fragment>
+        <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="center"
+            noWrap
+            sx={{ flex: 1 }}
+          >
+            {title}
+          </Typography>
+        </Toolbar>
+        <Toolbar
+          component="nav"
+          variant="dense"
+          sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+        >
+          {sections.map((section, index) => (
+            <Button
+              key={section.title}
+              color="inherit"
+              variant="text"
+              onClick={() => setSelectedSection(index)}
+              sx={{ p: 1, flexShrink: 0, ...(selectedSection === index && { backgroundColor: '#e0e0e0' }) }}
+            >
+              {section.title}
+            </Button>
+          ))}
+        </Toolbar>
+      </React.Fragment>
+    );
+  };
+  
   console.log("Data:", parsedAdopterData.role);
   let sections;
     if(parsedAdopterData.role == 0)  sections = staff;
