ab3db812b025b6e1c750d4641cf7cf10f3a2e8a6
sara-gaballa <saragaballa2002@gmail.com>
2023-12-30 11:28:58 +0200

    backup

--- PA_Backend/PetAdoptionSystem/src/main/java/com/example/PetAdoptionSystem/controller/BackupController.java
+++ PA_Backend/PetAdoptionSystem/src/main/java/com/example/PetAdoptionSystem/controller/BackupController.java
@@ -1,55 +1,53 @@
 package com.example.PetAdoptionSystem.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.example.PetAdoptionSystem.PetAdoptionSystemApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.CodeSource;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 
 @RestController
 @RequestMapping("/database")
 @CrossOrigin()
 public class BackupController {
 
-    private static final Logger logger = LoggerFactory.getLogger(BackupController.class);
-
     @PostMapping("/update")
     public ResponseEntity<String> systemUpdate() {
-        logger.info("Received backup request.");
+        System.out.println("Received backup request.");
 
         try {
-            // Getting path to the JAR file being executed
-            CodeSource codeSource = BackupController.class.getProtectionDomain().getCodeSource();
+            // Getting path to the JAR file being executed dynamically
+            CodeSource codeSource = PetAdoptionSystemApplication.class.getProtectionDomain().getCodeSource();
             File jarFile = new File(codeSource.getLocation().toURI().getPath());
-            String jarDir = "D:\\CSE25\\THIRD YEAR\\First term\\Database\\Project\\PetAdoption\\PA_Backend\\PetAdoptionSystem\\target\\PetAdoptionSystem-0.0.1-SNAPSHOT.jar";
-            logger.info("JAR file path: {}", jarFile.getAbsolutePath());
+            String jarDir = jarFile.getParentFile().getPath();
 
             // Creating Database Constraints
             String dbName = "pet_adoption_system";
             String dbUser = "root";
             String dbPass = "11112002";
 
-            // Creating Path Constraints for folder saving
-            String folderPath = jarDir + File.separator + "backup";
-            logger.info("Backup folder path: {}", folderPath);
+            // Creating Path Constraints for folder saving using Paths
+            Path folderPath = Paths.get(jarDir, "backup");
 
             // Creating Folder if it does not exist
-            File f1 = new File(folderPath);
+            File f1 = folderPath.toFile();
             f1.mkdirs();  // Use mkdirs() to create parent directories if they don't exist
 
-            // Creating Path Constraints for backup saving
-            String savePath = folderPath + File.separator + "backup.sql";
+            // Creating Path Constraints for backup saving using Paths
+            Path savePath = Paths.get(folderPath.toString(), "backup.sql");
 
             // Used to create a cmd command
-            String[] executeCmd = {"mysqldump", "-u" + dbUser, "-p" + dbPass, "--database", dbName, "-r", savePath};
+            String[] executeCmd = {"mysqldump", "-u" + dbUser, "-p" + dbPass, dbName, "-r", savePath.toString()};
 
             // Executing the command here
             ProcessBuilder processBuilder = new ProcessBuilder(executeCmd);
@@ -57,19 +55,80 @@ public class BackupController {
             int processComplete = runtimeProcess.waitFor();
 
             // processComplete=0 if correctly executed, will contain other values if not
-            logger.info("Executing mysqldump command: {}", String.join(" ", executeCmd));
-
             if (processComplete == 0) {
-                logger.info("Backup Complete");
+                System.out.println("Backup Complete");
                 return ResponseEntity.status(HttpStatus.CREATED).body("Backup Complete");
             } else {
-                logger.error("Backup Failure");
+                System.out.println("Backup Failure");
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Backup Failure");
             }
 
         } catch (URISyntaxException | IOException | InterruptedException ex) {
-            logger.error("Error at Backup_restore: {}", ex.getMessage());
             return new ResponseEntity<>("Error at Backup_restore: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
+    @PostMapping("/restore")
+    public ResponseEntity<String> systemRestore(@RequestParam("fileName") String fileName) {
+        try {
+            CodeSource codeSource = PetAdoptionSystemApplication.class.getProtectionDomain().getCodeSource();
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarDir = jarFile.getParentFile().getPath();
+
+            /* NOTE: Creating Database Constraints */
+            String dbName = "pet_adoption_system";
+            String dbUser = "root";
+            String dbPass = "11112002";
+
+            /* NOTE: Creating Path Constraints for restoring */
+            String restorePath = jarDir + "\\backup" + "\\" + fileName;
+            System.out.println(restorePath);
+            /* NOTE: Used to create a cmd command */
+            /* NOTE: Do not create a single large string, this will cause buffer locking, use string array */
+            String[] executeCmd = new String[]{"mysql", dbName, "-u" + dbUser, "-p" + dbPass, "-e", " source " + restorePath};
+
+            /* NOTE: processComplete=0 if correctly executed, will contain other values if not */
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            /* NOTE: processComplete=0 if correctly executed, will contain other values if not */
+            if (processComplete == 0) {
+                return ResponseEntity.ok("Successfully restored from SQL: " + fileName);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error at restoring");
+            }
+        } catch (URISyntaxException | IOException | InterruptedException | HeadlessException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error at Restoredbfromsql: " + ex.getMessage());
+        }
+    }
+
+    @GetMapping("/backupInfo")
+    public ResponseEntity<String> getBackupInfo() {
+        try {
+            CodeSource codeSource = PetAdoptionSystemApplication.class.getProtectionDomain().getCodeSource();
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarDir = jarFile.getParentFile().getPath();
+
+            // Path to the backup folder
+            String folderPath = jarDir + File.separator + "backup";
+            File backupFolder = new File(folderPath);
+
+            // Check if the backup folder exists and get its last modified timestamp
+            if (backupFolder.exists() && backupFolder.isDirectory()) {
+                long lastBackupTime = backupFolder.lastModified();
+
+                // Format the timestamp into a more readable date format
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String formattedDate = dateFormat.format(new Date(lastBackupTime));
+
+                return ResponseEntity.ok("Last Backup Time: " + formattedDate);
+            } else {
+                return ResponseEntity.ok("No backup performed yet");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching backup info: " + e.getMessage());
+        }
+    }
+
 }
--- PA_Frontend/pet-adoption-frontend/src/component/SystemBackup.js
+++ PA_Frontend/pet-adoption-frontend/src/component/SystemBackup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
@@ -15,12 +15,48 @@ export default function SystemBackup({ user }) {
         const result = await response.text();
         setBackupStatus(result);
       } else {
-        console.error('Backup request failed:', response.json());
+        console.error('Backup request failed:', response.statusText);
       }
     } catch (error) {
       console.error('Error during backup request:', error.message);
     }
   };
+
+  const handleRestoreClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8088/database/restore?fileName=backup.sql', {
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        const result = await response.text();
+        setBackupStatus(result);
+      } else {
+        console.error('Restore request failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error during restore request:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch existing backup information when the component mounts
+    const fetchBackupInfo = async () => {
+      try {
+        const response = await fetch('http://localhost:8088/database/backupInfo');
+        if (response.ok) {
+          const result = await response.text();
+          setBackupStatus(result);
+        } else {
+          console.error('Backup info request failed:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error during backup info request:', error.message);
+      }
+    };
+
+    fetchBackupInfo();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
     <div>
@@ -30,6 +66,12 @@ export default function SystemBackup({ user }) {
 
       <Box mt={2}>
         <strong>Last Backup Status:</strong> {backupStatus || 'No backup performed yet'}
+      </Box>
+
+      <Box mt={2}>
+        <Button onClick={handleRestoreClick} variant="contained" color="secondary">
+          Restore System
+        </Button>
       </Box>
     </div>
   );
